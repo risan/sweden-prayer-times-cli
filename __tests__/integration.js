279740d7a8687acefcb0210818bb730900878351
risan <risanbagja@gmail.com>
2018-11-10 18:29:42 +0100

    test: Test for missing city argument

--- __tests__/integration.js
+++ __tests__/integration.js
@@ -12,3 +12,13 @@ test("it can get prayer times", async () => {
   expect(stdout).toMatch(/maghrib[\s\S]+\d{2}:\d{2}/gim);
   expect(stdout).toMatch(/isha[\s\S]+\d{2}:\d{2}/gim);
 });
+
+test("it throws an error if city argument is missing", async () => {
+  expect.assertions(1);
+
+  try {
+    await execa("./src/index.js");
+  } catch (error) {
+    expect(error.stderr).toMatch(/city[\s\S]+missing/i);
+  }
+});
